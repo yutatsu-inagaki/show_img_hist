bd780395946fcad26e7867a2a1c09cebdcbd5e48
Yutatsu Inagaki <cmj14020@ict.nitech.ac.jp>
2022-07-23 17:15:44 +0900

    change agein

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from skimage.io import imread
 # download the image
 img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
 
-im = imread(img_url))
+im = imread(img_url)))
 
-st.image(im, caption = 'change the caption text',
-         use_column_width = True)
+st.image(im, caption='change the caption text',
+         use_column_width=True)
 
 
 # show histgram of all colors
-hist_red, _=np.histogram(im[:, :, 0], bins = 64)
-hist_green, _=np.histogram(im[:, :, 1], bins = 64)
-hist_blue, _=np.histogram(im[:, :, 2], bins = 64)
-hist=np.stack((hist_red, hist_green, hist_blue), axis = 1)
+hist_red, _=np.histogram(im[:, :, 0], bins=64)
+hist_green, _=np.histogram(im[:, :, 1], bins=64)
+hist_blue, _=np.histogram(im[:, :, 2], bins=64)
+hist=np.stack((hist_red, hist_green, hist_blue), axis=1)
 
-df_hist=pd.DataFrame(hist, columns = ['R', 'G', 'B'])
+df_hist=pd.DataFrame(hist, columns=['R', 'G', 'B'])
 st.bar_chart(df_hist)
 
 
@@ -28,11 +28,11 @@ color=st.radio(
     "choose R, G, or B",
     ('R', 'G', 'B'))
 if color == 'R':
-    df_hist = pd.DataFrame(hist_red)
+    df_hist=pd.DataFrame(hist_red)
     st.bar_chart(df_hist)
 if color == 'G':
-    df_hist = pd.DataFrame(hist_green)
+    df_hist=pd.DataFrame(hist_green)
     st.bar_chart(df_hist)
 if color == 'B':
-    df_hist = pd.DataFrame(hist_blue)
+    df_hist=pd.DataFrame(hist_blue)
     st.bar_chart(df_hist)
